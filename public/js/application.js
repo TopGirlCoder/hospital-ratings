4704aea25624f3f8410f50b365e10d838c8c4f5a
Jenna Andersen <jnicholea@gmail.com>
2015-11-27 11:54:11 -0800

    add google maps code and format hospitals index

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+	initialize();
+
 });
+
+function initialize(){
+        var latLon = {lat: 37.470246, lng: -121.912577};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: latLon,
+          zoom: 8
+        });
+
+        var geocoder = new google.maps.Geocoder();
+
+
+        geocoder.geocode( { 'address': '440 Tonopah Drive, Fremont, CA 94539'}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+ }
