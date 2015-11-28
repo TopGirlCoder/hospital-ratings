c51acba5b313812ef5d099f93c19ded37d3f160b
Kim Allen <kimmarieallen@gmail.com>
2015-11-27 16:21:42 -0800

    Get data from api, parse it, display it to a page

--- app/controllers/medicare_api_controller.rb
+++ app/controllers/medicare_api_controller.rb
@@ -1,11 +1,26 @@
 get '/medicare' do
- response = HTTParty.get('https://data.medicare.gov/resource/77hc-ibv8.json')
+ response = HTTParty.get('https://data.medicare.gov/resource/y7ru-x2zm.json?city=SAN%20FRANCISCO')
 
- # puts response.body, response.code, response.message, response.headers.inspect
-p response.body
-# p response.code
+# p response
+puts response.body, response.code, response.message, response.headers.inspect
+# puts response.body
+# @hospitals = array of hashes
+@sf_hospitals =[]
+@medicare_hospitals = JSON.parse(response.body)
+
+@medicare_hospitals.each do |hospital|
+  if hospital["measure_id"] == "HAI_2_SIR"
+    @sf_hospitals<<hospital
+    # @location = hospital["address"][""]
+  end
+end
+
+p @sf_hospitals
+erb :hospitals
+# puts response.code
 # p response.message
 # p response.headers.inspect
- # response.code.find_by(city: "San Francisco")
+ # response.body[0].where(city: "San Francisco")
+ # data_string = response.body
  #.where(measure_id: "HAI_2_SIR")
 end
